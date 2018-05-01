b73a3860c50035a26b84a7871281bb556f7cfb9f
Yann Mulonda <yannmjl@gmail.com>
2018-05-01 14:27:19 -0500

    added basic arithmetic operations

--- numeric.py
+++ numeric.py
@@ -1,3 +1,35 @@
 # Python 3.6 Tutorial for Beginners
 # Working with Numeric Data
 # Integers and Floats 
+
+# basic arithmetic operation
+num1 = 5
+num2 = 3
+# addition
+add = num1 + num2
+print('addtion:', add)
+# subtraction
+sub = num1 - num2
+print('subtraction:', sub)
+# multiplication
+multi = num1 * num2
+print('multiplication:', multi)
+# division
+div = num1 / num2
+print('division:', div)
+# floor division
+floor_div = num1 // num2
+print('floor division:', floor_div)
+# exponent
+expo = num1 ** num2
+print('exponent:', expo)
+# modulus
+modu = num1 % num2
+print('modulus', modu)
+
+# now let's see some method
+num = 4
+number = 3.14
+# 'type' shows the datatype
+print(type(num))
+print(type(number))
