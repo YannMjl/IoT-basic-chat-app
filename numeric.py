2f35b159c523cb1f69f6279024604eef149197fc
Yann Mulonda <yannmjl@gmail.com>
2018-05-01 15:05:57 -0500

    aded comapraison operations

--- numeric.py
+++ numeric.py
@@ -3,33 +3,67 @@
 # Integers and Floats 
 
 # basic arithmetic operation
+num = 4
 num1 = 5
 num2 = 3
+num3 = 3.68
+num4 = -6
+
 # addition
 add = num1 + num2
-print('addtion:', add)
+print(f'{num1} + {num2} =', add)
+
 # subtraction
 sub = num1 - num2
-print('subtraction:', sub)
+print(f'{num1} - {num2} =', sub)
+
 # multiplication
 multi = num1 * num2
-print('multiplication:', multi)
-# division
+print(f'{num1} * {num2} =', multi)
+
+# division in python 3 give us a decimal(float) 
+# eventhough we divided two intergers 
 div = num1 / num2
-print('division:', div)
-# floor division
+print(f'{num1} / {num2}=', div)
+
+# floor division drops the dicimal and only return an integer
 floor_div = num1 // num2
-print('floor division:', floor_div)
+print(f'{num1} // {num2} =', floor_div)
+
 # exponent
 expo = num1 ** num2
-print('exponent:', expo)
+print(f'{num1} ** {num2} =', expo)
+
 # modulus
 modu = num1 % num2
-print('modulus', modu)
+print(f'{num1} % {num2} =', modu)
 
 # now let's see some method
-num = 4
-number = 3.14
 # 'type' shows the datatype
 print(type(num))
-print(type(number))
+print(type(num3))
+
+# the order of operation 
+print(f'{num} * {num1} + {num2} =', num * num1 + num2)
+print(f'{num} * ({num1} + {num2}) =', num * (num1 + num2))
+
+# incremention, let's increment num by 1
+num += 1
+num1 *= 3
+print(f'num += 1 is:', num)
+print(f'num1 += 3 is:', num1)
+
+# absolute value
+print('num4 is:', num4)
+print('Absolute value of num4 is: ', abs(num4))
+
+# rouding
+print('num3 is:', num3)
+print('round of num3 is:', round(num3))
+# set how many digit we want to round to
+print(f'{num1} / {num2}=', div)
+print(f'{num1} / {num2} round to 2 =', round(div, 2))
+
+# comparaison operation: comparaison operators return boolean 'true' or 'false'
+# equal
+print(f'{num1} equal to {num2} ?', num1 == num2)
