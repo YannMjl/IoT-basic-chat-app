8ecebb03bd60b40ec504470e97ee15682ecd2de5
Yann Mulonda <yannmjl@gmail.com>
2018-05-01 11:55:12 -0500

    first commit

---
+++ intro.py
@@ -0,0 +1,16 @@
+# print welcome message
+messsage = 'hello world, Bleach is the best anime of all time'
+print(messsage)
+
+# print the lenght of the string message
+print('the lenght of the string message is: ', len(messsage))
+
+# print a specific caractere by passing an index
+print(messsage[23], messsage[38]) 
+
+# print a range of charactere in the string
+# this technic is called slicing
+print(messsage[0:11])
+# which is similar to 
+print(messsage[:11])
+
