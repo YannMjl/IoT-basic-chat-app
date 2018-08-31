3d3c1e9e0aa4de4b7431601928bead6dc1f423e2
Yann Mulonda <yannmjl@gmail.com>
2018-08-31 00:16:14 -0600

    Update standardplot.py

--- script/standardplot.py
+++ script/standardplot.py
@@ -2,6 +2,7 @@
 # now let's have a llok at something a little more insteresting
 # using matploting and NumPy
 # install matplotlib from termil by typing: pip3 install matplotlib
+
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
